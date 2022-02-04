6164879bd2ae2ef4c8a8868037983a724143f3c2
joaquin leonel <woriltomo10@gmail.com>
2022-02-04 18:58:25 -0300

    servidor express 2

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
-const express= require ('express');
+const express = require("express");
 const fs = require("fs");
-
-const PORT =process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-
-const productos =  fs.readFileSync("./productos.txt", "utf-8");
+const productos = fs.readFileSync("./productos.txt", "utf-8");
 const arrayProductos = JSON.parse(productos);
 
-
-function getRandom (min,max) {
-return Math.floor(Math.random()* (arrayProductos.length - 0)) + 0;
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (arrayProductos.length - 0)) + 0;
 }
 
 
-    app.get('/productos',(req,res)=>{
-     res.send(arrayProductos)
-     console.log(getRandom())
-        })
+app.get("/", (req, res) => {
+  res.send(`<h1 style="color:red;"> Bienvenidos a Express </h1>
+  <hr>
+  <h2 style="color:blue;"> Rutas disponibles: </h2>
+ <ul>
+  <li>/productos</li>
+  <li>/productosRandom</li>
+  <ul>
+  `);
 
-        app.get('/productosRandom',(req,res)=>{
-            res.send(arrayProductos[getRandom()])
-               })   
-
-
-app.listen(PORT, () =>{
-    console.log(`Servidor activo en el port ${PORT}`)
 });
 
+app.get("/productos", (req, res) => {
+  res.send(arrayProductos);
+  console.log(getRandom());
+});
+
+app.get("/productosRandom", (req, res) => {
+  res.send(arrayProductos[getRandom()]);
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor activo en el port ${PORT}`);
+});
 
